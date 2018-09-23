1f41044b30288b1d0343750977b0e7d0a12dafe2
David Buchanan <d@vidbuchanan.co.uk>
2018-09-23 02:26:23 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name="rc4",
+      version="1.0",
+      description="A simple binary-safe python3 implementation of the RC4 cipher",
+      author="David Buchanan",
+      url="https://github.com/DavidBuchanan314/rc4",
+      packages=["rc4"],
+      license="MIT",
+      )
